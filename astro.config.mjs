aa11f0cff66cdad28f4a3f5158b4d240654b2831
Nicolas Falabella <nicolasgfalabella@gmail.com>
2026-01-14 13:00:27 -0300

    Fix refresh dos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  
-  site: 'https://catalogo-26.vercel.app', 
-  
+  site: 'https://catalogo-26.vercel.app',
+
+  output: 'server',
+
+  adapter: vercel(),
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
